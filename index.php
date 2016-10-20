7b265ee44dcd1beec54b86401d9e5b155f3b37df
Primoz Cigler <primoz@proteusnet.com>
2016-10-20 13:57:25 +0200

    Add GA tracking script to error screen

--- index.php
+++ index.php
@@ -38,19 +38,33 @@ else {
 
 	if ( $EnvatoApi->get_number_of_errors() > 0 ) {
 		$logger->addNotice( 'Error screen shown when logging in.', [ 'number_of_errors' => $EnvatoApi->get_number_of_errors() ] );
+		$html = <<<EOHTML
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Error on Envato API</title>
+</head>
+<body style="background-color: #eee; color: #777; font-family: sans-serif; padding-top: 100px; line-height: 1.5;">
+	<div style="background-color: #fff; width: 500px; margin: 0 auto; border: 1px solid #ccc; padding: 30px 60px;">
+		<p>Yikes! Something went wrong with the Envato API, we were unable to sign you up. Till we resolve the issue, you can write directly to our support email (please include the purchase code): <strong>support@proteusthemes.zendesk.com</strong></p>
+	</div>
 
-		die( '
-		<html>
-		<head>
-			<meta charset="utf-8">
-			<title>Error on Envato API</title>
-		</head>
-		<body style="background-color: #eee; color: #777; font-family: sans-serif; padding-top: 100px; line-height: 1.5;">
-			<div style="background-color: #fff; width: 500px; margin: 0 auto; border: 1px solid #ccc; padding: 30px 60px;">
-				<p>Yikes! Something went wrong with the Envato API, we were unable to sign you up. Till we resolve the issue, you can write directly to our support email (please include the purchase code): <strong>support@proteusthemes.zendesk.com</strong></p>
-			</div>
-		</body>
-		</html>' );
+	<script>
+		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='https://www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		ga('create','UA-33538073-25','auto',{'allowLinker':true});
+		ga('require','linker');
+		ga('linker:autoLink',['proteusthemes.com','themeforest.net']);
+		ga('send','pageview');
+	</script>
+</body>
+</html>
+EOHTML;
+
+		die( $html );
 	}
 
 	$jwt = JWT::encode( $token, $key );
